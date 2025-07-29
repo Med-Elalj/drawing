883c5ecd823f3d111b294ca3782894925cd560b6
melalj <mohammedelalj@outlook.com>
2025-07-29 22:34:47 +0100

    sui

--- src/geometrical_shapes.rs
+++ src/geometrical_shapes.rs
@@ -1,16 +1,14 @@
+use rand::Rng;
 use raster::{Color, Image};
-use rand::Rng;  
 
 pub trait Drawable {
-    fn draw(&self,  image:&mut Image) {}
-    fn color(&self){}
+    fn draw(&self, image: &mut Image) {}
+    fn color(&self) {}
 }
-
 
 pub trait Displayable {
     fn display(&mut self, x: i32, y: i32, color: Color) {}
 }
-
 
 /*__________________________shapes_______________________*/
 // Poit
@@ -20,67 +18,66 @@ pub struct Point {
 }
 
 impl Point {
-    pub fn new(x:i32, y:i32)-> Self{
-        return Point{x: x, y:y};
+    pub fn new(x: i32, y: i32) -> Self {
+        return Point { x: x, y: y };
     }
-    pub fn random(width: i32, height: i32)-> Point{
+    pub fn random(width: i32, height: i32) -> Point {
         let mut rng = rand::rng();
 
-        let x : i32 = rng.random_range(0..width);
-        let y : i32 = rng.random_range(0..height);
+        let x: i32 = rng.random_range(0..width);
+        let y: i32 = rng.random_range(0..height);
         return Point::new(x, y);
     }
 
-    pub fn draw(self, image :&mut Image){
+    pub fn draw(self, image: &mut Image) {
         let red = Color::rgba(255, 0, 0, 255); // solid red
-        image.set_pixel(self.x,self.y, red).unwrap();
+        image.set_pixel(self.x, self.y, red).unwrap();
     }
 }
 // Line
 
-pub struct Line{
+pub struct Line {
     first: Point,
     last: Point,
 }
 
-impl Line{
-    pub fn new(x: Point, y: Point)-> Self{
-        return Line{first: x, last: y};
+impl Line {
+    pub fn new(x: Point, y: Point) -> Self {
+        return Line { first: x, last: y };
     }
 
-    pub fn random(width: i32, height: i32)-> Line {
-
-        let first : Point = Point::random(width, height); 
-        let last : Point = Point::random(width, height); 
-
+    pub fn random(width: i32, height: i32) -> Line {
+        let first: Point = Point::random(width, height);
+        let last: Point = Point::random(width, height);
+        println!(
+            "width {} -- height {} -- first.x {} -- first.y {} -- last.x {} -- last.y {} -- ",
+            width, height, first.x, first.y, last.x, last.y
+        );
         return Line::new(first, last);
     }
 
-    pub fn draw(&self, image :&mut Image){
+    pub fn draw(&self, image: &mut Image) {
         let red = Color::rgba(255, 0, 0, 255); // solid red
-        // fine the line equation 
-        // find a
-        let dx = self.first.x - self.last.x ;
-        let dy = self.first.y - self.last.y ;
-        let a = dx/dy;
-        // find b
-        let b = self.first.y + self.last.y; 
+                                               // fine the line equation
+                                               // find a
+        let ratio = self.ratio(image.width, image.height);
+        println!("{:?}",ratio);
 
-        for i in 0..image.width {
-            for j in 0..image.height {
-                if j == a*i + b /* && self.range(i , j)*/{
-                    image.set_pixel(i , j, red.clone()).unwrap();
-                }
-            }    
-        }
+        let to_max_h:f64 = image.height as f64 / image.width as f64 ;
+
+        for i in 0..=image.width {
+            let x: f64 = i as f64;
+            let y = (((j * to_max_h) * ratio.1) as i32 + self.first.y);
+            image.set_pixel((j * ratio.0) as i32 + self.first.x,y, red.clone())
+            .unwrap();
     }
-    pub fn range(&self, i : i32, j : i32 ) -> bool{
-        i > i32::min(self.first.x , self.last.x) && i < i32::max(self.first.x , self.last.x)
-        && j > i32::min(self.first.y , self.last.y) && i < i32::max(self.first.y , self.last.y)
+    // println!("{} {}",(j * ratio.0) as i32 + self.first.x,(j * ratio.1) as i32 + self.first.y);
+}
+    pub fn ratio(&self,width : i32, height: i32) -> (f64,f64) {
+        (((self.last.x-self.first.x) as f64) / width as f64,
+        ((self.last.y-self.first.y) as f64) / height as f64)
     }
 }
-
-
 
 // // Circle
 // pub struct Circle{
@@ -94,7 +91,7 @@ impl Line{
 //     }
 
 //     pub fn random(width: i32, height: i32)-> Circle{
-//         let c  : Point = Point::random(width, height); 
+//         let c  : Point = Point::random(width, height);
 //         let mut rng = rand::rng();
 
 //         let r = rng.random_range(0..height) as u32;
